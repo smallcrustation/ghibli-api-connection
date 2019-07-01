9919800ccde128ad5854b6a15301f91046e37081
krill <lpmaze@gmail.com>
2019-07-01 13:10:37 -0400

    Humans added, width on small screen fixed

--- main.js
+++ main.js
@@ -35,6 +35,7 @@ function ghibliApiPromise(endpoint) {
 
 function handleMenuSelection() {
 
+  // --- get all films and display ---
   $('#js-data-films').on('click', (e) => {
 
     // prevent page from scrolling to top
@@ -45,7 +46,7 @@ function handleMenuSelection() {
 
     // get object list of films, call a promise using  Promises and .then
     ghibliApiPromise('films').then(JSON.parse).then(function (response) {
-      console.log('Success!');
+      console.log('Success! films');
       // handleDisplayFilms()
       handleDisplayFilms(response);
 
@@ -54,16 +55,94 @@ function handleMenuSelection() {
     });
   });
 
+  // ---- get all cats and display -----
   $('#js-data-species-cat').on('click', (e) => {
     // prevent page from scrolling to top
     e.preventDefault();
-    console.log('js-cat');
+
+    $('.js-ghibli-data-type').text('Cats');
+
+    ghibliApiPromise('species?name=Cat').then(JSON.parse).then(function (response) {
+      console.log('Success! cats');
+      let catsList = response[0].people; // bc response is an array even if just 1 item returned
+
+      $('#js-content').empty(); // empty the html b4 going through the list and adding new cats
+
+      for (let i = 0; i < catsList.length; i++) {
+        let catApiUrl = catsList[i];
+        catApiUrl = catApiUrl.slice(32); // b/c ghibliApiPromise() already has that
+        ghibliApiPromise(catApiUrl).then(JSON.parse).then(function (catResponse) {
+          //console.log('Success! got a cat');
+
+          // check if cat.films is greater than 0
+          if (catResponse.films.length > 0) {
+            // each cat only has one film
+            const filmUrl = catResponse.films[0].slice(32);
+            // console.log(filmUrl);
+            ghibliApiPromise(filmUrl).then(JSON.parse).then(function (filmResponse) {
+              // console.log(filmResponse.title);
+              handleDisplayCat(catResponse, filmResponse);
+            }, function (error) {
+              console.error('error', error);
+            });
+
+          }
+          else {
+            console.log('no films for this cat');
+            handleDisplayCat(catResponse);
+          }
+          // handleDisplayCat(catResponse);
+
+        }, function (error) {
+          console.error('Failed!', error);
+        });
+      }
+
+    }, function (error) {
+      console.error('Failed!', error);
+    });
   });
 
-  $('#js-data-species-people').on('click', (e) => {
+  $('#js-data-species-human').on('click', (e) => {
     // prevent page from scrolling to top
     e.preventDefault();
-    console.log('js-people');
+
+    $('.js-ghibli-data-type').text('Humans');
+
+    ghibliApiPromise('species?name=Human').then(JSON.parse).then(function (response) {
+      console.log('Success! humans');
+
+      $('#js-content').empty();
+
+      let humanUrlList = response[0].people;
+
+      for (let i = 0; i < humanUrlList.length; i++) {
+        let humanUrl = humanUrlList[i].slice(32);
+        // GOOD TIL HERE
+        // go to each human url to get it's data
+        ghibliApiPromise(humanUrl).then(JSON.parse).then(function (humanResponse) {
+          // get human movie url, [0] b/c ea. are only in one movie
+          //console.log(humanResponse);
+          let filmUrl = humanResponse.films[0].slice(32);
+
+          ghibliApiPromise(filmUrl).then(JSON.parse).then(function (filmResponse) {
+            // console.log(humanResponse.name + ' ' + filmResponse.title);
+
+
+            handleDisplayHuman(humanResponse, filmResponse);
+
+          }, function (error) {
+            console.error('Error', error);
+          });
+
+        }, function (error) {
+          console.error('Error', error);
+        });
+      }
+    }, function (error) {
+      console.error('Error', error);
+    });
+
   });
 }
 
@@ -73,7 +152,7 @@ window.onload = function () {
   $('.js-ghibli-data-type').text('Films');
   // get object list of films, call a promise using  Promises and .then
   ghibliApiPromise('films').then(JSON.parse).then(function (response) {
-    console.log('Success!');
+    console.log('Success! films');
     // handleDisplayFilms()
     handleDisplayFilms(response);
 
@@ -96,10 +175,57 @@ function handleDisplayFilms(filmsList) {
     </section>`;
     });
 
+    $('#js-content').empty();
     $('#js-content').append(html);
-
-    
   }
+}
+
+function handleDisplayCat(cat, film) {
+
+  if (cat.age === 'NA') {
+    cat.age = 'Unknown';
+  }
+
+  let color = getRandomColor();
+
+  let html = `<section class="card" style="border-color:${color};">
+  <h2 style="background-color:${color};">${cat.name}</h2>
+  <article>
+  <ul>
+    <li>Age: ${cat.age}</li>
+    <li>Sex: ${cat.gender}</li>
+    <li>Eye Color: ${cat.eye_color}</li>
+    <li>Hair Color: ${cat.hair_color}</li> 
+    <li>Film: ${film.title}</li>
+  </ul>  
+  </article>
+</section>`;
+
+  $('#js-content').append(html);
+}
+
+function handleDisplayHuman(human, film) {
+
+  if (human.age === 'NA') {
+    human.age = 'Unknown';
+  }
+
+  let color = getRandomColor();
+
+  let html = `<section class="card" style="border-color:${color};">
+  <h2 style="background-color:${color};">${human.name}</h2>
+  <article>
+  <ul>
+    <li>Age: ${human.age}</li>
+    <li>Sex: ${human.gender}</li>
+    <li>Eye Color: ${human.eye_color}</li>
+    <li>Hair Color: ${human.hair_color}</li> 
+    <li>Film: ${film.title}</li>
+  </ul>  
+  </article>
+</section>`;
+
+  $('#js-content').append(html);
 }
 
 
